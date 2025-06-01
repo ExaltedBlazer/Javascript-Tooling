7b6e9815e9a0c1dcc956a304129427ddcbd09703
Your Name <death5819@gmail.com>
2025-05-31 23:29:14 -0400

    5/31 code

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 //Importing module
-import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+//import { addToCart, totalPrice, totalQuantity } from '/shoppingCart.js';
 
 console.log('Importing Module');
 //console.log(shippingCost);
 
-addToCart('bread', 5);
-console.log();
+//addToCart('bread', 5);
+//console.log(totalPrice, totalQuantity);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
+
+//console.log('Start fetching');
+//const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//const data = await res.json();
+//console.log(data);
+//console.log('true');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+//lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,6 +1,11 @@
 //Exporting module
 console.log('Exporting module');
 
+//Blocking code
+console.log('Start fetching users');
+await fetch('https://jsonplaceholder.typicode.com/users');
+console.log('Finish fetching users');
+
 const shippingCost = 10;
 const cart = [];
 
@@ -13,3 +18,8 @@ const totalPrice = 237;
 const totalQuantity = 23;
 
 export { totalPrice, totalQuantity };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log('${quantity} ${product} added to caart');
+}
